e0a511e78a5f233d4688b2e63f5694617b371723
Magnus Hjelmen Kristiansen <s333740@oslomet.no>
2020-09-08 11:46:15 +0200

    endring på hvordan stasjon brukes

--- Gruppeoppgave1/wwwroot/js/index.js
+++ Gruppeoppgave1/wwwroot/js/index.js
@@ -8,6 +8,11 @@ function hentAlleStasjoner() {
     });
 }
 
+var stasjon = {
+
+}
+
+
 //Autocompleter søk på stasjoner
 function stasjonerAutocomplete(stasjoner) {
     $(".stasjonerAutocomplete").autocomplete({
@@ -20,7 +25,8 @@ function stasjonerAutocomplete(stasjoner) {
                 if (navn.indexOf(request.term.toUpperCase()) != -1) {
                     return {
                         label: obj.navn,
-                        value: obj.id
+                        value: obj.id,
+                        obj: obj
                     }
                 } else {
                     return null;
@@ -35,38 +41,69 @@ function stasjonerAutocomplete(stasjoner) {
         //Hva som skjer når en stasjon blir valgt
         //ui.item er objektet valgt i dropdown, den har to attributter. Label som er teksten(navn) og value som er indeks i db
         select: function (event, ui) {
-            event.preventDefault();
-            var divId = $(this).attr("id");
-            var retning = divId.substring(0, 3);
-            var stasjonsId = ui.item.value;
+            var stasjon = ui.item.obj;
+            //var stasjonsId = ui.item.value;
+            //var stasjonsNavn = ui.item.label;
 
-            lagDestinasjonsBoks(retning, stasjonsId);
+            stasjonsValg(stasjon);
             return false;
         }
     });
 }
 
+//Velger stasjoner og lagrer hvilken stasjon som er valgt som til eller fra
+function stasjonsValg(stasjon) {
+    var step = sessionStorage.getItem("step");
+ 
+        //Det er første stasjons valg
+        if (step === null || step === 1) {
+            sessionStorage.setItem("fra", stasjon.navn);
+            sessionStorage.setItem("step", 1);
+            lagDestinasjonsBoksFra(stasjon);
+        }
+        //Andre valg
+        else {
+            sessionStorage.setItem("til", stasjon.navn);
+            sessionStorage.setItem("step", 2);
+            lagDestinasjonsBoksTil(stasjon);
+        }
+}
+
+
 //Lager bokser etter at man har valg en stasjon enten fra eller til
-function lagDestinasjonsBoks(retning, stasjonsId) {
-    $("#" + retning + "Søk").hide("slow");
-    const url = "Stasjon/HentEnStasjon?id=" + stasjonsId;
+function lagDestinasjonsBoksFra(stasjon) {
+    $("#fraSøk").hide("slow");
     var ut = "";
 
-    $.get(url, function (stasjon) {
-        ut += '<b style="font-size:3em;">' + retning.toUpperCase() + ': </b>' +
-              '<b style="font-size:2.5em;">' + stasjon.navn + '</b>' +
-              '<button class="btn btn-danger" id="test" style="display:flex;justify-content:flex-end;align-items:center"' +
-              'onclick="endreStasjon(\'' + retning + '\')" > Endre</button > </br > ';
+    //var stasjon = sessionStorage.getItem("fra");
 
-        $("#" + retning + "Boks").html(ut);
-    });
+    ut += '<b style="font-size:3em;">Fra: </b>' +
+          '<b style="font-size:2.5em;">' + stasjon.navn + '</b>' +
+          '<button class="btn btn-danger" style="display:flex;justify-content:flex-end;align-items:center"' +
+          'onclick="endreStasjon(fra)">Endre</button></br>';
 
-    //Lagre info i div
-    $("#" + retning + "Boks")
-        .data("retning", retning)
-        .data("stasjonsid", stasjonsId);
+    $("#fraBoks").html(ut);
+    $("#fraBoks").show("slow");
 
-    $("#" + retning + "Boks").show("slow");
+    //Hvis både til og fra stasjon er valgt vil de neste instillingene komme opp
+    if (($("#tilBoks").is(":visible")) && ($("#fraBoks").is(":visible"))) {
+        lagBestillingBoks();
+    }
+}
+
+
+function lagDestinasjonsBoksTil(stasjon) {
+    $("#tilSøk").hide("slow");
+    var ut = "";
+    //var stasjon = sessionStorage.getItem("til");
+
+    ut += '<b style="font-size:3em;">Til: </b>' +
+          '<b style="font-size:2.5em;">' + stasjon.navn + '</b>' +
+          '<button class="btn btn-danger" style="display:flex;justify-content:flex-end;align-items:center"' +
+          'onclick="endreStasjon(til)">Endre</button></br>';
+
+    $("#tilBoks").html(ut);
+    $("#tilBoks").show("slow");
 
     //Hvis både til og fra stasjon er valgt vil de neste instillingene komme opp
     if (($("#tilBoks").is(":visible")) && ($("#fraBoks").is(":visible"))) {
@@ -79,7 +116,7 @@ function lagBestillingBoks() {
     $("#bestillingsBoks").show("slow");
     $("#datovalg").datepicker();
 
-    lagBestilling();
+    //lagBestilling(stasjonFraId, stasjonTilId, dato, tidspunkt);
 }
 
 //Når alle valg er utført lager vi en bestilling og pusher til database
@@ -94,18 +131,15 @@ function endreStasjon(retning) {
     //Fjerner tekst fra tidligere
     $(".stasjonerAutocomplete").val("");
 
-    //Gjemmer og tar frem relevante divs
-    $("#" + retning + "Søk").show("slow");
-    $("#" + retning + "Boks").hide();
-}
+    console.log(retning);
 
-function test() {
-    $.get("Stasjon/Test", function (OK) {
-        if (OK) {
-            $("#feil").html("Return true");
-        }
-        else {
-            $("#feil").html("Return false");
-        }
-    });
+    if (retning == "fra") {
+        $("#fraSøk").show("slow");
+        $("#fraBoks").hide();
+        sessionStorage.setItem("step", 1);
+    } else{
+        $("#tilSøk").show("slow");
+        $("#tilBoks").hide();
+        sessionStorage.setItem("step", 1);
+    }
 }
