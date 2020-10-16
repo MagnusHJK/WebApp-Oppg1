3f0a8d322af5edde6797a56a923d2fa70e448e3e
martinjohansen <s333727@oslomet.no>
2020-10-16 16:53:38 +0200

    lagd noen tester, ikke ferdig

--- XUnitTestGruppeoppgave1/BrukerTest.cs
+++ XUnitTestGruppeoppgave1/BrukerTest.cs
@@ -1,10 +1,47 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Net;
 using System.Text;
+using System.Threading.Tasks;
+using Gruppeoppgave1.Controllers;
+using Gruppeoppgave1.DAL;
+using Gruppeoppgave1.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Moq;
+using Xunit;
 
 namespace XUnitTestGruppeoppgave1
 {
-    class BrukerTest
+    public class BrukerTest
     {
+        private readonly Mock<IBrukerRepository> mockRep = new Mock<IBrukerRepository>();
+        private readonly Mock<ILogger<BrukerController>> mockLog = new Mock<ILogger<BrukerController>>();
+
+        [Fact]
+        public async Task LoggInnTrue()
+        {
+            mockRep.Setup(s => s.LoggInn(It.IsAny<Bruker>())).ReturnsAsync(true);
+
+            var BrukerController = new BrukerController(mockRep.Object, mockLog.Object);
+
+            var resultat = await BrukerController.LoggInn(It.IsAny<Bruker>()) as OkObjectResult;
+
+            Assert.Equal((int)HttpStatusCode.OK, resultat.StatusCode);
+            Assert.Equal(true, resultat.Value);
+        }
+
+        [Fact]
+        public async Task LoggInnFalse()
+        {
+            mockRep.Setup(s => s.LoggInn(It.IsAny<Bruker>())).ReturnsAsync(false);
+
+            var BrukerController = new BrukerController(mockRep.Object, mockLog.Object);
+
+            var resultat = await BrukerController.LoggInn(It.IsAny<Bruker>()) as OkObjectResult;
+
+            Assert.Equal((int)HttpStatusCode.NotFound, resultat.StatusCode);
+            Assert.Equal("Innlogging feilet", resultat.Value);
+        }
     }
 }
